392a2098887dbae78beff001578639b0985aefb7
itsralia <justforralia@gmail.com>
2022-04-04 00:34:46 +0100

    third blog commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { PossibleFragmentSpreadsRule } from 'graphql'
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import  {PostCard}  from '../components'
@@ -11,7 +10,7 @@ import { FeaturedPosts } from '../sections'
 
 
 
-export default function Home({ posts }){
+export default function Home( { posts }){
   return (
     <div className="container  mx-auto  px-10 mb-8 ">
       <Head>
@@ -19,20 +18,19 @@ export default function Home({ posts }){
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FeaturedPosts />
-    <div className='grid-cols-1 grid lg:grid-cols-12 gap-12 '>
-    <div className="lg:col-span-8 col-span-1">
-      {posts.map((post) =>( <PostCard post={ post.node } key={post.title} />))}
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
-     
-    <div className='lg:col-span-4 col-span-1'>
-    <div className='lg:sticky relative top-8'>
-        <PostWidget />
-        <Categories />
-    </div>
-    
-    
-    </div>
-    </div>
     </div>
   );
 }
